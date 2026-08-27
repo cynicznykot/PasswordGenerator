6ad38a24352604daed440f0a72be0fcbb375a752
Gleb Krotov <iotajot@gmail.com>
2026-08-27 20:22:47 +0500

    test: add tests for save_passwords_csv and save_passwords_json

--- tests/test_function.py
+++ tests/test_function.py
@@ -4,7 +4,8 @@ Test script for the generator module.
 
 import os
 import csv
-from src.generator import save_passwords_csv
+import json
+from src.generator import save_passwords_csv, save_passwords_json
 
 def test_save_passwords_csv():
     """Test save_passwords_csv function."""
@@ -25,10 +26,35 @@ def test_save_passwords_csv():
         assert len(rows) == 2, "Wrong number of rows!"
         assert rows[0]["service"] == "Google", "Wrong service name!"
 
-    print("✅ All tests passed!")
+    print("✅ All tests for csv format passed!")
 
     os.remove(file_path)
     print("🗑️ Test file removed.")
 
+def test_save_passwords_json():
+    """Test save_passwords_json function."""
+    test_passwords = [
+            {"service": "Google", "login": "user@gmail.com", "password": "P@ssw0rd!"},
+            {"service": "GitHub", "login": "username", "password": "Abc123!"}
+    ]
+
+    file_path = "test_passwords.json"
+
+    save_passwords_json(file_path, test_passwords)
+
+    assert os.path.exists(file_path), "File was not created!"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        rows = json.load(f)
+        assert len(rows) == 2, "Wrong number of rows!"
+        assert rows[0]["service"] == "Google", "Wrong service name!"
+
+    print("✅ All tests for json format passed!")
+
+    os.remove(file_path)
+    print("🗑️  Test file removed.")
+
 if __name__ == "__main__":
     test_save_passwords_csv()
+    test_save_passwords_json()
+
